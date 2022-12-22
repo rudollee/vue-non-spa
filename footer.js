7a11e4fffadf1d51084f86fb7758a0901a26ea18
wook@desktop05 <rudollee@gmail.com>
2022-12-22 10:23:12 +0900

    first commit

---
+++ footer.js
@@ -0,0 +1,13 @@
+export default {
+    name: 'footer',
+
+    setup() {
+        const title = "Non SPA";
+        const bodies = 'This is about Non-SPA Vue setting';
+        
+        return {
+            title,
+            body: bodies
+        };
+    },
+}
---
+++ main.js
@@ -0,0 +1,14 @@
+export default {
+    name: 'main',
+
+    setup() {
+        const title = "Non-SPA Vue";
+        const bodies = 'This is Non-SPA Vue Tutorial';
+        
+        return {
+            title,
+            body: bodies
+        };
+    },
+
+};
